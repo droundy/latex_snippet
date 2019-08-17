d850f8a89bdf89aa3e7ec676968c0ca322f89790
David Roundy <roundyd@physics.oregonstate.edu>
2019-08-17 06:47:05 -0700

    skip figure placement parameters

--- src/lib.rs
+++ src/lib.rs
@@ -525,6 +525,9 @@ pub fn html_paragraph(fmt: &mut impl std::io::Write,
                         if name.chars().last() != Some('}') {
                             fmt_errors(fmt, &[r"\begin", name])?;
                         } else if name == "{figure}" {
+                            // Just skip any figure placement parameters
+                            let opt = optional_argument(latex);
+                            latex = &latex[opt.len()..];
                             if let Some(i) = latex.find(r"\end{figure}") {
                                 fmt.write_all(b"<figure>")?;
                                 html_paragraph(fmt, &latex[..i])?;
